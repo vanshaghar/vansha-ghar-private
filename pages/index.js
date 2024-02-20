b82888aca6e93cb9f1453ff29cc66f209521483a
Kishor U <blackkspydo@gmail.com>
2024-02-21 00:04:39 +0545

    Add Tailwind CSS and update team section

--- pages/index.js
+++ pages/index.js
@@ -367,132 +367,38 @@ const Index = () => {
           style={{ backgroundImage: "url(assets/images/bg/dot-bg.png)" }}
         />
         <div className="container">
-          <div className="row align-items-center">
+          <div className="row w-full align-items-center">
             <div className="col-xl-4 col-lg-12">
               {/*=== Chef Content Box ===*/}
               <div className="chef-content-box content-box-gap mb-30 pr-lg-30 wow fadeInLeft">
                 <div className="section-title section-title-left mb-30">
                   <span className="sub-title">Best food menu</span>
-                  <h2>Meet Our Exclusive &amp; Master Chefs</h2>
+                  <h2>Meet our Passionate Team</h2>
                 </div>
                 <ul className="check-style-one">
-                  <li>25 Years of Experience in Restaurant Services in USA</li>
+                  <li>A Symphony of Culinary Experts and Hospitality Maestros</li>
                   <li>
-                    Any Kind Of Food Made For Customer and So Much Yamee AED  Testy
+                    Crafters of Your Exceptional Dining Moments
+                  </li>
+                  <li>
+                    United in Crafting Unforgettable Gastronomic Journeys
                   </li>
                 </ul>
                 <p>
-                  Sit amet consectetur adipiscing elitsue risus mauris quam
-                  adipiscing phasellus aene ante orcirat scelerisque enim ut
-                  nulla
+                  At the core of our restaurant lies our dedicated team — a blend of chefs, sommeliers, and service staff who bring to life the essence of our cuisine and the spirit of our hospitality. Their combined expertise and passion are poured into every dish, every recommendation, every interaction. Together, they create more than just meals; they create experiences that linger on the palate and in memory.
                 </p>
                 <Link legacyBehavior href="/chefs">
                   <a className="main-btn filled-btn">
-                    See more about chefs
+                    See more about our Team
                     <i className="far fa-arrow-right" />
                   </a>
                 </Link>
               </div>
             </div>
             <div className="col-xl-8 col-lg-12">
-              <div className="row">
-                <div className="col-xl-4 col-md-6 col-sm-12">
-                  {/*=== Single Team Item ===*/}
-                  <div className="single-team-item mb-30 wow fadeInUp">
-                    <div className="chef-img">
-                      <img
-                        src="assets/images/team/chef-1.jpg"
-                        alt="Chef Image"
-                      />
-                      <div className="chef-overlay" />
-                      <div className="hover-content">
-                        <h3 className="title">Jimmie K. Cryer</h3>
-                        <p className="position">Senior Chef</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-xl-4 col-md-6 col-sm-12">
-                  {/*=== Single Team Item ===*/}
-                  <div className="single-team-item mb-30 wow fadeInUp">
-                    <div className="chef-img">
-                      <img
-                        src="assets/images/team/chef-2.jpg"
-                        alt="Chef Image"
-                      />
-                      <div className="chef-overlay" />
-                      <div className="hover-content">
-                        <h3 className="title">Jimmie K. Cryer</h3>
-                        <p className="position">Senior Chef</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-xl-4 col-md-6 col-sm-12">
-                  {/*=== Single Team Item ===*/}
-                  <div className="single-team-item mb-30 wow fadeInUp">
-                    <div className="chef-img">
-                      <img
-                        src="assets/images/team/chef-3.jpg"
-                        alt="Chef Image"
-                      />
-                      <div className="chef-overlay" />
-                      <div className="hover-content">
-                        <h3 className="title">Jimmie K. Cryer</h3>
-                        <p className="position">Senior Chef</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-xl-4 col-md-6 col-sm-12">
-                  {/*=== Single Team Item ===*/}
-                  <div className="single-team-item mb-30 wow fadeInUp">
-                    <div className="chef-img">
-                      <img
-                        src="assets/images/team/chef-4.jpg"
-                        alt="Chef Image"
-                      />
-                      <div className="chef-overlay" />
-                      <div className="hover-content">
-                        <h3 className="title">Jimmie K. Cryer</h3>
-                        <p className="position">Senior Chef</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-xl-4 col-md-6 col-sm-12">
-                  {/*=== Single Team Item ===*/}
-                  <div className="single-team-item mb-30 wow fadeInUp">
-                    <div className="chef-img">
-                      <img
-                        src="assets/images/team/chef-5.jpg"
-                        alt="Chef Image"
-                      />
-                      <div className="chef-overlay" />
-                      <div className="hover-content">
-                        <h3 className="title">Jimmie K. Cryer</h3>
-                        <p className="position">Senior Chef</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-xl-4 col-md-6 col-sm-12">
-                  {/*=== Single Team Item ===*/}
-                  <div className="single-team-item mb-30 wow fadeInUp">
-                    <div className="chef-img">
-                      <img
-                        src="assets/images/team/chef-6.jpg"
-                        alt="Chef Image"
-                      />
-                      <div className="chef-overlay" />
-                      <div className="hover-content">
-                        <h3 className="title">Jimmie K. Cryer</h3>
-                        <p className="position">Senior Chef</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <img src="https://images.pexels.com/photos/3184393/pexels-photo-3184393.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                className=" w-full h-full wow fadeInRight"
+                alt="groupphoto" />
             </div>
           </div>
         </div>
