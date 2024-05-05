2de24be9f198f02a857af8149941365093ab3671
Kishor U <blackkspydo@gmail.com>
2024-05-05 22:07:23 +0545

    Update CSS and images for banner and menu sections

--- pages/menu.js
+++ pages/menu.js
@@ -3,12 +3,11 @@ import PageBanner from "../src/components/PageBanner";
 import Skyline from "../src/components/Skyline";
 import Layout from "../src/layout/Layout";
 import { useEffect, useState } from "react";
-
+import ScrollToTop from "../src/components/ScrollToTop";
 
 export async function getStaticProps() {
 
     const graphqlEndpoint = process.env.NEXT_PUBLIC_API_URL;
-    console.log(graphqlEndpoint);
 
     const res = await fetch(graphqlEndpoint, {
         method: 'POST',
@@ -20,64 +19,65 @@ export async function getStaticProps() {
         body: JSON.stringify({
             query: `
           query {
-            Menus(limit: 1000) {
-              docs {
-                name
-                id
-                description
-                price
-                category {
+            OrganizedMenus(limit: 100) {
+                docs {
+                  id
                   name
-                  value
+                  category {
+                    id
+                    name
+                    value
+                  }
+                  menus {
+                    menu {
+                      name
+                      id
+                      description
+                      price
+                      foodType {
+                        type
+                        value
+                      }
+                      image {
+                        url
+                        alt
+                      }
+                      ingredients
+                    }
+                    index
+                  }
                 }
-                foodType {
-                  type
-                  value
-                }
-                image {
-                  url
-                  alt
-                }
-                ingredients
               }
-            }
           }
         `
         })
     }).then((response) => response.json());
 
-    const menus = res.data?.Menus?.docs ?? [];
+    const menus = res.data?.OrganizedMenus?.docs ?? [];
+    function formatMenu(data) {
+        const formattedData = data.sort((a, b) => {
+            if (a.category.index > b.category.index) return 1;
+            if (a.category.index < b.category.index) return -1;
+            return 0;
+        }).map(menu => {
+            const { menus, ...rest } = menu;
+            return {
+                ...rest,
+                category: {
+                    ...menu.category,
+                    link: `#${menu.category.name.toLowerCase().replace(/\s/g, '-')}`
+                },
+                menus: menus.sort((a, b) => {
+                    if (a.index > b.index) return 1;
+                    if (a.index < b.index) return -1;
+                    return 0;
+                }).map(menu => menu.menu)
+            };
+        });
 
-    const formattedMenu = menus && menus.length ?
-        menus.reduce((acc, menu) => {
-            const category = menu?.category?.name || 'No Category';
-            const existingCategory = acc.find(item => item?.type === category);
-
-            if (existingCategory) {
-                existingCategory.items.push({
-                    name: menu?.name || '',
-                    price: menu?.price || 0,
-                    description: menu?.description || '',
-                    ingredients: menu?.ingredients || []
-                });
-            } else {
-                acc.push({
-                    type: category,
-                    link: `#${category.toLowerCase().replace(/\s/g, '-')}`,
-                    items: [
-                        {
-                            name: menu?.name || '',
-                            price: menu?.price || 0,
-                            description: menu?.description || '',
-                            ingredients: menu?.ingredients || []
-                        }
-                    ]
-                });
-            }
-
-            return acc;
-        }, []) : [];
-
+        return formattedData;
+    }
+    const formattedMenu = formatMenu(menus);
 
     const categories = Array.from(new Set(menus.map(menu => menu?.category?.name ?? "No Category"))).map(category => ({
         type: category,
@@ -94,20 +94,17 @@ const Menu = ({
     menus, categories
 }) => {
     const [activeCategory, setActiveCategory] = useState('');
-
-    useEffect(() => {
-        console.log({ menus, categories });
-    });
     return (
         <Layout>
             <PageBanner pageName={"Menu"} title="Menu" />
+            <ScrollToTop />
             <div className="mx-auto py-10">
 
                 <section
-                    className="sticky py-1  top-0 z-50 bg-white "
+                    className="sticky py-1 top-0 z-50 bg-white "
                 >
                     <div className="">
-                        <div className="flex flex-nowrap justify-start md:justify-center hide-scrollbar items-center overflow-x-auto">
+                        <div className="flex max-w-[1300px] flex-nowrap mx-auto  justify-start  hide-scrollbar items-center overflow-x-auto">
                             {
                                 categories.map((category, index) => (
                                     <div
@@ -130,18 +127,18 @@ const Menu = ({
                     </div>
                 </section>
                 <section
-                    className=" rounded-lg  my-4"
+                    className="rounded-lg my-4"
                 >
-                    <div className="w-screen relative">
+                    <div className="w-screen max-w-[1300px] mx-auto relative">
                         {
-                            menus.map((category, index) => (
+                            menus.map((item, index) => (
                                 <div
-                                    key={category.type}
+                                    key={item.category.name}
                                     className={`w-full my-[200px] ${index === 0 && "mt-5"}`}
-                                    id={category.link?.replace("#", "")}
+                                    id={item.category.link?.replace("#", "")}
                                 >
-                                    <h2 className="text-4xl relative hidden md:block w-[400px] mx-auto text-center p-3 font-bold mb-1" >
-                                        {category.type}
+                                    <h2 className="text-4xl relative hidden md:block mx-auto text-center p-3 font-bold mb-1" >
+                                        {item.category.name}
                                         <span className="absolute top-1/2 left-0 w-1/4 h-[2px] bg-gray-900 transform -translate-y-1/2" />
                                         <span className="absolute top-1/2 right-0 w-1/4 h-[2px] bg-gray-900 transform -translate-y-1/2" />
                                     </h2>
@@ -151,7 +148,7 @@ const Menu = ({
                                                 style={{
                                                     lineHeight: "1",
                                                 }}
-                                            >{category.type}</div>
+                                            >{item.category.name}</div>
 
                                         </div>
                                         <div className="h-full w-full  relative flex items-center justify-center overflow-hidden  rounded-lg"
@@ -161,9 +158,9 @@ const Menu = ({
                                     </div>
                                     <div className="grid px-3 grid-cols-1 gap-1 !gap-x-3 md:grid-cols-2 place-items-center place-content-center">
                                         {
-                                            category.items.map((item, index) => (
+                                            item?.menus?.map((menuitem, index) => (
                                                 <div
-                                                    key={item.name}
+                                                    key={menuitem?.name ?? index}
                                                     className="flex w-full flex-col  items-start bg-white rounded-lg md:flex-col md:max-w-xl dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
 
                                                     <div className="w-full flex gap-4 flex-row  justify-between relative mb-4">
@@ -173,20 +170,20 @@ const Menu = ({
                                                         />
 
                                                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-                                                            {item.name}
+                                                            {menuitem?.name}
                                                         </h5>
 
                                                         <span className="text-xl font-bold flex-shrink-0 text-gray-900">
-                                                            AED {item.price}
+                                                            AED {menuitem?.price}
                                                         </span>
 
 
                                                     </div>
                                                     <p className="mb-3 font-sans italic text-[12px] text-gray-700 line-clamp-2">
-                                                        {item.ingredients}
+                                                        {menuitem?.ingredients}
                                                     </p>
                                                     <p className="mb-3 font-sans text-sm text-gray-700 line-clamp-2 hidden lg:block">
-                                                        {item.description}
+                                                        {menuitem?.description}
                                                     </p>
                                                 </div>
                                             ))
