62a96990f8a2c3160ae872cc7b4796a34fc87550
Kishor U <blackkspydo@gmail.com>
2024-06-17 12:50:13 +0545

    fixed popup

--- src/components/sections/Popup.jsx
+++ src/components/sections/Popup.jsx
@@ -24,7 +24,7 @@ const Popup = ({
 
     return (
         <div className="fixed bg-black bg-opacity-25 z-[1000] inset-0 flex items-center justify-center ">
-            <div className="bg-white rounded-lg shadow-lg  p-3 max-w-2xl mx-2 sm:mx-auto relative">
+            <div className="bg-white rounded-lg shadow-lg my-5 p-3 max-w-2xl mx-2 sm:mx-auto relative">
                 <button
                     className="absolute -top-2 -right-2 text-gray-700"
                     onClick={handleClose}
