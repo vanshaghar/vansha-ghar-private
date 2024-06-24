9fee8043f2bec8bfc219a00fc60de5b6cd0855c5
Kishor U <blackkspydo@gmail.com>
2024-06-25 00:36:55 +0545

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -22,63 +22,65 @@ const App = ({ Component, pageProps }) => {
       <Head>
         <title>Vansha Ghar</title>
         {/*====== Favicon Icon ======*/}
-        <link
-          rel="shortcut icon"
-          href="/assets/images/favicon.ico"
-          type="image/png"
-        />
-        {/*====== Flaticon css ======*/}
-        <link rel="stylesheet" href="/assets/fonts/flaticon/flaticon.css" />
-        {/*====== FontAwesome css ======*/}
-        <link
-          rel="stylesheet"
-          href="/assets/fonts/fontawesome/css/all.min.css"
-        />
-        {/*====== Bootstrap css ======*/}
-        <link
-          rel="stylesheet"
-          href="/assets/vendor/bootstrap/css/bootstrap.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="node_modules/sassy-datepicker/dist/styles.css"
-        />
-        {/*====== magnific-popup css ======*/}
-        <link
-          rel="stylesheet"
-          href="/assets/vendor/magnific-popup/dist/magnific-popup.css"
-        />
-        {/*====== Slick-popup css ======*/}
-        <link rel="stylesheet" href="/assets/vendor/slick/slick.css" />
-        {/*====== Jquery UI css ======*/}
-        <link
-          rel="stylesheet"
-          href="/assets/vendor/jquery-ui/jquery-ui.min.css"
-        />
-        {/*====== Nice Select css ======*/}
-        <link
-          rel="stylesheet"
-          href="/assets/vendor/nice-select/css/nice-select.css"
-        />
-        {/*====== Animate css ======*/}
-        <link rel="stylesheet" href="/assets/vendor/animate.css" />
-        {/*====== Default css ======*/}
-        <link rel="stylesheet" href="/assets/css/default.css" />
-        {/*====== Style css ======*/}
-        <link rel="stylesheet" href="/assets/css/style.css" />
-      </Head>
-      <div
-        className={`transition-opacity ${loader ? "opacity-0" : "opacity-100"}`}
-      >
-        <Component {...pageProps} />
-      </div>
-      <FloatingWhatsApp
-        phoneNumber="+971524729073"
-        accountName="Vansha Ghar"
-        avatar="/assets/images/logo/logo-white-bg.jpg"
-        chatMessage="Hi, How can I help you?"
-      />
-    </Fragment>
-  );
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png/"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff"/>
+            {/*====== Flaticon css ======*/}
+            <link rel="stylesheet" href="/assets/fonts/flaticon/flaticon.css" />
+            {/*====== FontAwesome css ======*/}
+            <link
+              rel="stylesheet"
+              href="/assets/fonts/fontawesome/css/all.min.css"
+            />
+            {/*====== Bootstrap css ======*/}
+            <link
+              rel="stylesheet"
+              href="/assets/vendor/bootstrap/css/bootstrap.min.css"
+            />
+            <link
+              rel="stylesheet"
+              href="node_modules/sassy-datepicker/dist/styles.css"
+            />
+            {/*====== magnific-popup css ======*/}
+            <link
+              rel="stylesheet"
+              href="/assets/vendor/magnific-popup/dist/magnific-popup.css"
+            />
+            {/*====== Slick-popup css ======*/}
+            <link rel="stylesheet" href="/assets/vendor/slick/slick.css" />
+            {/*====== Jquery UI css ======*/}
+            <link
+              rel="stylesheet"
+              href="/assets/vendor/jquery-ui/jquery-ui.min.css"
+            />
+            {/*====== Nice Select css ======*/}
+            <link
+              rel="stylesheet"
+              href="/assets/vendor/nice-select/css/nice-select.css"
+            />
+            {/*====== Animate css ======*/}
+            <link rel="stylesheet" href="/assets/vendor/animate.css" />
+            {/*====== Default css ======*/}
+            <link rel="stylesheet" href="/assets/css/default.css" />
+            {/*====== Style css ======*/}
+            <link rel="stylesheet" href="/assets/css/style.css" />
+          </Head>
+          <div
+            className={`transition-opacity ${loader ? "opacity-0" : "opacity-100"}`}
+          >
+            <Component {...pageProps} />
+          </div>
+          <FloatingWhatsApp
+            phoneNumber="+971524729073"
+            accountName="Vansha Ghar"
+            avatar="/assets/images/logo/logo-white-bg.jpg"
+            chatMessage="Hi, How can I help you?"
+          />
+        </Fragment>
+        );
 };
-export default App;
+        export default App;
